366a38a5069edb735c8ca4c63da4b1aadeafd0e3
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2025-07-05 01:55:20 +0300

    test: add jest

---
+++ jest.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  testMatch: ["**/__tests__/**/*.test.ts", "**/?(*.)+(spec|test).ts"],
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts", "!src/index.ts"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
+  moduleNameMapper: {
+    "^@app-types/(.*)$": "<rootDir>/src/types/$1",
+    "^@config/(.*)$": "<rootDir>/src/config/$1",
+    "^@parsers/(.*)$": "<rootDir>/src/parsers/$1",
+    "^@utils/(.*)$": "<rootDir>/src/utils/$1"
+  },
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"]
+};
